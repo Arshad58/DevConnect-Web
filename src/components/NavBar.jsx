418f605d73d51b150ec80522bbdb22ffbc80e50c
Arshad <attararshad004@gmail.com>
2026-03-03 15:45:35 +0530

    login auth validation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
@@ -6,43 +7,43 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm px-4">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">DevConnect</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          DevConnect
+        </Link>
       </div>
-        {user && (
-          <div className="flex items-center gap-4">
-            <span className="font-medium">
-              Welcome, {user.firstName}
-            </span>
-            <div className="dropdown dropdown-end">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle avatar"
-              >
-                <div className="w-10 rounded-full">
-                  <img alt="User Photo" src={user.photourl} />
-                </div>
+      {user && (
+        <div className="flex items-center gap-4">
+          <span className="font-medium">Welcome, {user.firstName}</span>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="User Photo" src={user.photourl} />
               </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-              >
-                <li>
-                  <a className="justify-between">
-                    Profile 
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <a>Settings</a>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-              </ul>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
           </div>
-        )}
+        </div>
+      )}
     </div>
   );
 };
